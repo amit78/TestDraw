633534836386cb913c8ba97b240c60d7dcf1d22c
amit_qlogy <amit.j.joglekar@gmail.com>
2013-12-03 13:26:33 +0530

    Initial commit

--- src/com/Amit/MyLine.java
+++ src/com/Amit/MyLine.java
@@ -1,7 +1,71 @@
 package com.Amit;
 
+import java.awt.*;
+
 /**
  * Created by qlogy on 12/3/13.
  */
 public class MyLine {
+
+    private int y1;
+    private int x2;
+    private int y2;
+    private int x1;
+
+    public MyLine() {
+        x1 = 0;
+        y1 = 0;
+        x2 = 0;
+        y2 = 0;
+
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public MyLine(int x1, int y1, int x2, int y2) {
+
+        setX1(x1);
+        setY1(y1);
+        setX2(x2);
+        setY2(y2);
+    }
+
+
+    private void setX1(int x1) {
+        this.x1 = (x1 >= 0 ? x1 : 0);
+
+    }
+
+    private void setY1(int y1) {
+        this.y1 = (y1 >= 0 ? y1 : 0);
+
+    }
+
+    private void setX2(int x2) {
+        this.x2 = (x2 >= 0 ? x2 : 0);
+
+    }
+
+    private void setY2(int y2) {
+        this.y2 = (y2 >= 0 ? y2 : 0);
+
+    }
+
+    public void draw(Graphics g) {
+        g.drawLine(x1, y1, x2, y2);
+    }
 }
--- src/com/Amit/TestDraw.java
+++ src/com/Amit/TestDraw.java
@@ -1,12 +1,31 @@
 package com.Amit;
 
 import javax.swing.*;
+import java.awt.*;
 
-/**
- * Created by qlogy on 12/3/13.
- */
 public class TestDraw extends JApplet {
-    public void init(){
+    private MyLine[] line;
 
+    public void init() {
+
+        line = new MyLine[5];
+
+
+        for (int count = 0; count <= line.length; count++) {
+            int x1 = (int) (Math.random() * 400);
+            int y1 = (int) (Math.random() * 400);
+            int x2 = (int) (Math.random() * 400);
+            int y2 = (int) (Math.random() * 400);
+
+            line[count] = new MyLine(x1, y1, x2, y2);
+        }
+
+
+    }
+
+    public void paint(Graphics g) {
+        for (int count = 0; count < line.length; count++) {
+            line[count].draw(g);
+        }
     }
 }
