b80344890ae5409e6df0b6d1ca620568573212be
amit_qlogy <amit.j.joglekar@gmail.com>
2013-12-03 13:04:46 +0530

    Initial commit

---
+++ src/com/Amit/MyOval.java
@@ -0,0 +1,7 @@
+package com.Amit;
+
+/**
+ * Created by qlogy on 12/3/13.
+ */
+public class MyOval {
+}
---
+++ src/com/Amit/TestDraw.java
@@ -0,0 +1,12 @@
+package com.Amit;
+
+import javax.swing.*;
+
+/**
+ * Created by qlogy on 12/3/13.
+ */
+public class TestDraw extends JApplet {
+    public void init(){
+
+    }
+}
